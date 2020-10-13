9f9205ab49d6c205fab312d021cdc7c978a27998
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-10-13 20:58:46 -0300

    Style popup on map

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,1 +1,27 @@
-const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+//create map
+const map = L.map('mapid').setView([-23.5197659,-46.8122576], 14);
+
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//creat icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    IconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+//create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
+
+//create an add marker
+L.marker([-23.5197659,-46.8122576], {icon})
+.addTo(map)
+.bindPopup(popup);
