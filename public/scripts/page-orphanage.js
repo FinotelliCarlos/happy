b8bbaff45effe1f3b38f92d3af4219c78c459780
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-10-13 23:36:27 -0300

    added functions to images

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -28,5 +28,23 @@ L.marker([-23.5197659, -46.8122576], { icon })
 //image gallery
 
 function selectImage(event){
-  
+  const button = event.currentTarget
+
+  //retirar all classes active
+  const buttons = document.querySelectorAll('.images button')
+  buttons.forEach(removeActiveClass)
+
+  function removeActiveClass(button){
+    button.classList.remove('active')
+  }
+
+  //selecionar a imagem clicada
+  const image = button.children[0]
+  const imageContainer = document.querySelector('.orphanage-details > img')
+
+  //atualizar o container de imagem
+  imageContainer.src = image.src
+
+  //adicionar a classe active ao botao clicado
+  button.classList.add('active')
 }
