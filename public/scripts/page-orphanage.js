0b22cf81797aceea4ea3b9454daa5c9abb3c8ea4
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-10-13 23:37:26 -0300

    format documents

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,17 +1,16 @@
 const options = {
-    dragging: false,
-    touchZoom: false,
-    doubleClickZoom: false,
-    scrollWheelZoom: false,
-    zoomControl: false
-}
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
+};
 
 //create map
 const map = L.map("mapid", options).setView([-23.5197659, -46.8122576], 14);
 
 //create and add tileLayer
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
-.addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 //creat icon
 const icon = L.icon({
@@ -22,29 +21,28 @@ const icon = L.icon({
 });
 
 //create an add marker
-L.marker([-23.5197659, -46.8122576], { icon })
-.addTo(map);
+L.marker([-23.5197659, -46.8122576], { icon }).addTo(map);
 
 //image gallery
 
-function selectImage(event){
-  const button = event.currentTarget
+function selectImage(event) {
+  const button = event.currentTarget;
 
   //retirar all classes active
-  const buttons = document.querySelectorAll('.images button')
-  buttons.forEach(removeActiveClass)
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach(removeActiveClass);
 
-  function removeActiveClass(button){
-    button.classList.remove('active')
+  function removeActiveClass(button) {
+    button.classList.remove("active");
   }
 
   //selecionar a imagem clicada
-  const image = button.children[0]
-  const imageContainer = document.querySelector('.orphanage-details > img')
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orphanage-details > img");
 
   //atualizar o container de imagem
-  imageContainer.src = image.src
+  imageContainer.src = image.src;
 
   //adicionar a classe active ao botao clicado
-  button.classList.add('active')
+  button.classList.add("active");
 }
