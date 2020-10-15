5001114f6f58719a91429c9b9ccbffb8c7f0c0ae
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-10-15 20:12:44 -0300

    criando rota

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 //importar dependencia
 const express = require('express');
+const path = require('path');
 
 //iniciando o express
 const server = express();
+server
+    //utilizando os arquivos estáticos
+    .use(express.static('public'))
 
-//criar uma rota
-server.get('/', () => {
+    //criar uma rota
+    .get('/', (request, response) => {
+        return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+    });
 
-});
-
+    
 //server on
 server.listen(5500);
