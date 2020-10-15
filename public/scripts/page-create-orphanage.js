44ac0f86b706248d7084e2f7073a70db3d3f5bbd
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-10-14 22:18:51 -0300

    format documents

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -2,8 +2,7 @@
 const map = L.map("mapid").setView([-23.5197659, -46.8122576], 14);
 
 //create and add tileLayer
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
-.addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 //creat icon
 const icon = L.icon({
@@ -12,9 +11,7 @@ const icon = L.icon({
   IconAnchor: [29, 68],
 });
 
-
 let marker;
-
 
 //create an add marker
 map.on("click", (event) => {
@@ -28,59 +25,60 @@ map.on("click", (event) => {
   marker && map.removeLayer(marker);
 
   //add icon layer
-  marker = L.marker([lat, lng], { icon })
-  .addTo(map);
+  marker = L.marker([lat, lng], { icon }).addTo(map);
 });
 
 //adicionar o campo de fotos
 function addPhotoField() {
-    // pear o container de fotos #images
-    const container =  document.querySelector('#images')
-    // pegar o container para duplicar .new-upload
-    const fieldsContainer = document.querySelectorAll('.new-upload')
-    // realizar o clone da ultima imagem adicionada
-    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
-    //verificar se o campo esta vazio, se sim não adicionar ao container de imagens
-    const input = newFieldContainer.children[0]
+  // pear o container de fotos #images
+  const container = document.querySelector("#images");
+  // pegar o container para duplicar .new-upload
+  const fieldsContainer = document.querySelectorAll(".new-upload");
+  // realizar o clone da ultima imagem adicionada
+  const newFieldContainer = fieldsContainer[
+    fieldsContainer.length - 1
+  ].cloneNode(true);
+  //verificar se o campo esta vazio, se sim não adicionar ao container de imagens
+  const input = newFieldContainer.children[0];
 
-    if(input.value == "" ){
-        return
-    }
-    // limpar o campo antes de adicionar ao container de imagens
-    newFieldContainer.children[0].value = ""
-    // adicionar o clone ao container de imagens
-    container.appendChild(newFieldContainer)
+  if (input.value == "") {
+    return;
+  }
+  // limpar o campo antes de adicionar ao container de imagens
+  newFieldContainer.children[0].value = "";
+  // adicionar o clone ao container de imagens
+  container.appendChild(newFieldContainer);
 }
 
-function deleteField(event){
-    const span = event.currentTarget
+function deleteField(event) {
+  const span = event.currentTarget;
 
-    const fieldsContainer = document.querySelectorAll('.new-upload')
+  const fieldsContainer = document.querySelectorAll(".new-upload");
 
-    if(fieldsContainer.length <= 1){
-        //limpar o valor do campo
-        span.parentNode.children[0].value = ""
-        return
-    }
+  if (fieldsContainer.length <= 1) {
+    //limpar o valor do campo
+    span.parentNode.children[0].value = "";
+    return;
+  }
 
-    //deletar o campo
-    span.parentNode.remove()
+  //deletar o campo
+  span.parentNode.remove();
 }
 
 //troca do sim e não
 
-function toggleSelect(event){
-
+function toggleSelect(event) {
   //retirar a class .active dos botoes
-  document.querySelectorAll('.button-select button')
-  .forEach((button) => button.classList.remove('active'))
+  document
+    .querySelectorAll(".button-select button")
+    .forEach((button) => button.classList.remove("active"));
 
   //colocar a class .active nesse botão clicado
-  const button = event.currentTarget
-  button.classList.add('active')
+  const button = event.currentTarget;
+  button.classList.add("active");
 
   //atualizar o meu input com o valor selecionado
-  const input = document.querySelector('[name="open_on_weekends"]')
+  const input = document.querySelector('[name="open_on_weekends"]');
 
-  input.value = button.dataset.value
+  input.value = button.dataset.value;
 }
