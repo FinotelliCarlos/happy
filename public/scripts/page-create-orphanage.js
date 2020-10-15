f39fba2b2a7d95da18a27181bbdf4720655c4d0c
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-10-14 21:20:17 -0300

    added function on insert container new-upload

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -2,33 +2,48 @@
 const map = L.map("mapid").setView([-23.5197659, -46.8122576], 14);
 
 //create and add tileLayer
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
-.addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 //creat icon
 const icon = L.icon({
   iconUrl: "./public/images/map-marker.svg",
   iconSize: [58, 68],
   IconAnchor: [29, 68],
-
 });
 
 let marker;
 
 //create an add marker
-map.on('click', (event) => {
-    const lat = event.latlng.lat;
-    const lng = event.latlng.lng;
+map.on("click", (event) => {
+  const lat = event.latlng.lat;
+  const lng = event.latlng.lng;
 
-    //remove icon
-    marker && map.removeLayer(marker)
+  document.querySelector("[name=lat]").value = lat;
+  document.querySelector("[name=lng]").value = lng;
 
-    //add icon layer
-    marker = L.marker([lat,lng], {icon})
-    .addTo(map);
+  //remove icon
+  marker && map.removeLayer(marker);
 
+  //add icon layer
+  marker = L.marker([lat, lng], { icon }).addTo(map);
 });
 
+//adicionar o campo de fotos
+function addPhotoField() {
+    // pear o container de fotos #images
+    const container =  document.querySelector('#images')
+    // pegar o container para duplicar .new-upload
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+    // realizar o clone da ultima imagem adicionada
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+    //verificar se o campo esta vazio, se sim não adicionar ao container de imagens
+    const input = newFieldContainer.children[0]
 
-//L.marker([-23.5197659, -46.8122576], { icon })
-//.addTo(map);
+    if(input.value == "" ){
+        return
+    }
+    // limpar o campo antes de adicionar ao container de imagens
+    input.value == ""
+    // adicionar o clone ao container de imagens
+    container.appendChild(newFieldContainer)
+}
