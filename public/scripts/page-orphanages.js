38675d2700dee3b3832c277a0309f1781046d2c2
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-10-15 22:14:20 -0300

    format page-orphanages

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -12,9 +12,7 @@ const icon = L.icon({
   popupAnchor: [170, 2],
 });
 
-
-function addMarker({id,name,lat,lng}) {
-
+function addMarker({ id, name, lat, lng }) {
   //create popup overlay
   const popup = L.popup({
     closeButton: false,
@@ -26,19 +24,17 @@ function addMarker({id,name,lat,lng}) {
   );
 
   //create an add marker
-  L.marker([lat, lng], { icon })
-  .addTo(map)
-  .bindPopup(popup)
+  L.marker([lat, lng], { icon }).addTo(map).bindPopup(popup);
 }
 
-const orphanagesSpan = document.querySelectorAll('.orphanages span')
-orphanagesSpan.forEach(span => {
+const orphanagesSpan = document.querySelectorAll(".orphanages span");
+orphanagesSpan.forEach((span) => {
   const orphanage = {
     id: span.dataset.id,
     name: span.dataset.name,
     lat: span.dataset.lat,
-    lng: span.dataset.lng
-  }
+    lng: span.dataset.lng,
+  };
 
-  addMarker(orphanage)
-})
+  addMarker(orphanage);
+});
