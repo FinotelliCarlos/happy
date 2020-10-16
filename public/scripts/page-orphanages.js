640e7d7642218f46bdf4913acdf38d9506adee62
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-10-15 22:04:47 -0300

    acrescentar marker no mapa

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -12,15 +12,33 @@ const icon = L.icon({
   popupAnchor: [170, 2],
 });
 
-//create popup overlay
-const popup = L.popup({
-  closeButton: false,
-  className: "map-popup",
-  minWidth: 240,
-  minHeight: 240,
-}).setContent(
-  'Lar das meninas <a href="orphanage?id=1" class="choose-orphanage"> <img src="/images/arrow-white.svg"></a>'
-);
 
-//create an add marker
-L.marker([-23.5197659, -46.8122576], { icon }).addTo(map).bindPopup(popup);
+function addMarker({id,name,lat,lng}) {
+
+  //create popup overlay
+  const popup = L.popup({
+    closeButton: false,
+    className: "map-popup",
+    minWidth: 240,
+    minHeight: 240,
+  }).setContent(
+    `${name} <a href="orphanage?id=${id}" class="choose-orphanage"> <img src="/images/arrow-white.svg"></a>`
+  );
+
+  //create an add marker
+  L.marker([lat, lng], { icon })
+  .addTo(map)
+  .bindPopup(popup)
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+orphanagesSpan.forEach(span => {
+  const orphanage = {
+    id: span.dataset.id,
+    name: span.dataset.name,
+    lat: span.dataset.lat,
+    lng: span.dataset.lng
+  }
+
+  addMarker(orphanage)
+})
