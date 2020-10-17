f195f55c2263eba5b4bd1981969e84daed33a246
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-10-17 09:50:39 -0300

    operações no banco de dados

--- src/database/test.js
+++ src/database/test.js
@@ -1,36 +1,29 @@
 const Database = require('./db');
+const saveOrphanage = require('./saveOrphanage')
 
 Database.then(async db => {
+    
     //inserir dado na tabela 
-   await db.run(`
-        INSERT INTO orphanages (
-            lat,
-            lng,
-            name,
-            about,
-            whatsapp,
-            images,
-            instructions,
-            opening_hours,
-            open_on_weekends
-        ) VALUES (
-            "-23.5197659",
-            "-46.8522576",
-            "Lar dos meninos",
-            "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
-            "11972388905",
-            "https://images.unsplash.com/photo-1600711725407-2ea4733a38c3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-            "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
-            "Horario de Visitas das 18h até 8h",
-            "0"
-        );
-    `)
+   await saveOrphanage(db, {
+    lat: "-23.5197659",
+    lng: "-46.8922576",
+    name: "Lar de amor",
+    description:"Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images:[
+        "https://images.unsplash.com/photo-1600711725407-2ea4733a38c3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+        "https://images.unsplash.com/photo-1570570626315-95c19358f053?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    ].toString(),
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horario de Visitas das 18h até 8h",
+    open_on_weekends: "1"
+},)
 
     //consultar dados da tabela
     const selectedOrphanages = await db.all("SELECT * FROM orphanages")
     console.log(selectedOrphanages)
 
-    consultar somente 1 orphanato pelo id
-    const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "2"')
+    //consultar somente 1 orphanato pelo id
+    const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3"')
     console.log(orphanage)
 })
