880b3761eff73c57447efd115efbcf91b42b6a20
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-10-17 10:01:42 -0300

    delete id: 4,5,6

--- src/database/test.js
+++ src/database/test.js
@@ -8,7 +8,8 @@ Database.then(async db => {
     lat: "-23.5197659",
     lng: "-46.8922576",
     name: "Lar de amor",
-    description:"Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    about:"Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    whatsapp: "121212121",
     images:[
         "https://images.unsplash.com/photo-1600711725407-2ea4733a38c3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
 
@@ -24,6 +25,9 @@ Database.then(async db => {
     console.log(selectedOrphanages)
 
     //consultar somente 1 orphanato pelo id
-    const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3"')
+    const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "#"')
     console.log(orphanage)
+
+    //deletar dado da tabela
+    console.log(await db.run('DELETE FROM orphanages where ID = "#"'))
 })
