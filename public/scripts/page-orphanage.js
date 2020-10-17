7578797aae7337d877e03a77922fd8fcf4174d9f
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2020-10-17 18:15:12 -0300

    format scripts documents

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -7,15 +7,14 @@ const options = {
 };
 
 //get values from html
-const lat = document.querySelector('span[data-lat]').dataset.lat
-const lng = document.querySelector('span[data-lng]').dataset.lng
+const lat = document.querySelector("span[data-lat]").dataset.lat;
+const lng = document.querySelector("span[data-lng]").dataset.lng;
 
 //create map
 const map = L.map("mapid", options).setView([lat, lng], 15);
 
 //create and add tileLayer
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
-.addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 //creat icon
 const icon = L.icon({
@@ -26,9 +25,7 @@ const icon = L.icon({
 });
 
 //create an add marker
-L
-.marker([lat, lng], { icon })
-.addTo(map);
+L.marker([lat, lng], { icon }).addTo(map);
 
 //image gallery
 
